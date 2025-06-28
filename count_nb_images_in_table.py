ebdee8c5ed9d44dcd5915a541223b21a8e075a3c
SyRoxS1 <leo.moreaumenanteau@gmail.com>
2025-06-28 18:55:19 +0200

    fix bug

--- count_nb_images_in_table.py
+++ count_nb_images_in_table.py
@@ -1,14 +1,13 @@
 from dtb_con import get_db_connection
 
-def count_images(number):
+def count_images():
 
     conn = get_db_connection()
     cursor = conn.cursor()
     
     # Insert the image path and game name into the database
     cursor.execute(
-        "SELECT count(image_path) FROM image_game_link;",
-        (number)
+        "SELECT count(image_path) FROM image_game_link;"
     )
     
     result = cursor.fetchone()
