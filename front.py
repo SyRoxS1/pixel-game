67a3a53920f3a89a82837fb904cfb8299c96f49d
SyRoxS1 <leo.moreaumenanteau@gmail.com>
2025-06-28 19:26:25 +0200

    refractor

--- front.py
+++ front.py
@@ -14,8 +14,10 @@ def index():
         random_game_id = random.randint(0, count_images())
         original_image_path = select_image(request.cookies.get("game"))
         print(f"Selected image path: {original_image_path}")
-        pixelize_image(original_image_path, "tmp/"+str(request.cookies.get("game"))+ str(attempt_count) +".jpg",attempt_count)
-        image = convert_image_to_base64("tmp/"+request.cookies.get("game")+ attempt_count +".jpg")
+    
+        pixelize_image_path = "tmp/"+str(request.cookies.get("game"))+ str(attempt_count) +".jpg"
+        pixelize_image(original_image_path, pixelize_image_path, attempt_count)
+        image = convert_image_to_base64(pixelize_image_path)
         response = make_response(render_template('index.html',b64_img = image))
         response.set_cookie("game", str(random_game_id), max_age=60*60*24)
         return response
