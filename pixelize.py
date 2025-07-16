6469ae9ae552c63f699b5fe1ee20f3099df459f6
SyRoxS1 <leo.moreaumenanteau@gmail.com>
2025-07-16 22:30:12 +0200

    fix bug

--- pixelize.py
+++ pixelize.py
@@ -2,14 +2,22 @@ from PIL import Image
 
 def pixelize_image(input_path, output_path, pixel_size):
     img = Image.open(input_path)
-    # Resize down
+
+
     small = img.resize(
         (img.size[0] // pixel_size, img.size[1] // pixel_size),
         resample=Image.BILINEAR
     )
-    # Scale back up
+
+
     result = small.resize(img.size, Image.NEAREST)
+
+
+    if output_path.lower().endswith(('.jpg', '.jpeg')) and result.mode in ('RGBA', 'LA'):
+        result = result.convert('RGB')
+
     result.save(output_path)
+
 
 
 
